7db872126a916f9d673a05bb68d6f1251d68ef69
leandrogavidia <leandergs1234@gmail.com>
2022-11-24 17:53:08 -0400

    Remove console.log functions

--- contracts/DecentralizedJournal.sol
+++ contracts/DecentralizedJournal.sol
@@ -2,8 +2,6 @@
 
 pragma solidity >= 0.7.0 < 0.9.0;
 
-import "hardhat/console.sol";
-
 contract DecentralizedJournal {
     struct note {
         string title;
@@ -23,45 +21,29 @@ contract DecentralizedJournal {
         );
         
         journals[msg.sender].push(newNote);
-        console.log("Hi, you added:", _title);
     }
 
     function getJournal() public view returns(note[] memory) {
-        console.log("Hi, you all note", journals[msg.sender].length);
         return journals[msg.sender];
     }
 
     function getOneNote(uint _noteId) public view returns(note memory) {
-        console.log(
-            journals[msg.sender][_noteId - 1].title,
-            journals[msg.sender][_noteId - 1].content,
-            journals[msg.sender][_noteId - 1].date,
-            journals[msg.sender][_noteId - 1].id
-        );
-
         return journals[msg.sender][_noteId - 1];
     }
 
     function getTotalNotes() public view returns(uint256) {
-        console.log("You have:", journals[msg.sender].length, msg.sender);
         return journals[msg.sender].length;
     }
 
     function changeNoteTitle(uint256 _id, string memory _newTitle) public {
-        console.log(journals[msg.sender][_id - 1].title);
         journals[msg.sender][_id - 1].title = _newTitle;
-        console.log(journals[msg.sender][_id - 1].title);
     }
 
     function changeNoteContent(uint256 _id, string memory _newContent) public {
-        console.log(journals[msg.sender][_id - 1].content);
         journals[msg.sender][_id - 1].content = _newContent;
-        console.log(journals[msg.sender][_id - 1].content);
     }
 
     function deleteJournal() public {
-        console.log("You are deleting your journal", getTotalNotes());
         delete journals[msg.sender];
-        console.log("Journal delete", getTotalNotes());
     }
 }
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,54 +1,9 @@
 const deploy = async () => {
-    const [owner, randomPerson] = await hre.ethers.getSigners();
     const DecentralizedJournalFactory = await hre.ethers.getContractFactory("DecentralizedJournal");
     const DecentralizedJournal = await DecentralizedJournalFactory.deploy();
     await DecentralizedJournal.deployed();
 
-    // Add one note
-
-    let addNote = await DecentralizedJournal.addNote("Primera nota", "Holaaaaaa");
-    await addNote.wait();
-
-    let getJournal = await DecentralizedJournal.getJournal();
-
-    // Change title
-
-    let getOneNote = await DecentralizedJournal.getOneNote(1);
-
-    let changeTitleNote = await DecentralizedJournal.changeNoteTitle(1, "Nueva caja de herramientas");
-    await changeTitleNote.wait();
-
-    getOneNote = await DecentralizedJournal.getOneNote(1);
-
-    // Change content
-
-    let changeNoteContent = await DecentralizedJournal.changeNoteContent(1, "Nuevo contenido");
-    await changeNoteContent.wait();
-
-    getOneNote = await DecentralizedJournal.getOneNote(1);
-
-    // Delete journal
-
-    getJournal = await DecentralizedJournal.getJournal();
-
-    addNote = await DecentralizedJournal.addNote("Nota 2", "Contenido 2")
-    await addNote.wait();
-
-    addNote = await DecentralizedJournal.addNote("Nota 3", "Contenido 3")
-    await addNote.wait();
-
-    getJournal = await DecentralizedJournal.getJournal();
-
-    let deleteJournal = await DecentralizedJournal.deleteJournal();
-    await deleteJournal.wait();
-
-    getJournal = await DecentralizedJournal.getJournal();
-
-    // addNote = await DecentralizedJournal.addNote("Primera nota", "Holaaaaaa");
-    // await addNote.wait();
-
-    // deleteJournal = await DecentralizedJournal.deleteJournal();
-    // await deleteJournal.wait();
+    console.log("Contract address:", DecentralizedJournal.address);
 }
 
 const runDeploy = async () => {
