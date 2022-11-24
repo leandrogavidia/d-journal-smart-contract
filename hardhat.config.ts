0b225c354ee8d39176cbf99b00ceb95a46ea8c1f
leandrogavidia <leandergs1234@gmail.com>
2022-11-24 18:17:52 -0400

    Add config to deploy contract to BNBChain testnet and console.log functions on deploy.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,15 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+require("dotenv").config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  networks: {
+    bnbchainTestnet: {
+      url: process.env.STAGING_QUICKNODE_KEY,
+      accounts: [process.env.PRIVATE_KEY as string]
+    }
+  }
 };
 
 export default config;
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,8 +1,12 @@
 const deploy = async () => {
+    const [deployer] = await hre.ethers.getSigners();
+    const accountBalance = await deployer.getBalance();
     const DecentralizedJournalFactory = await hre.ethers.getContractFactory("DecentralizedJournal");
     const DecentralizedJournal = await DecentralizedJournalFactory.deploy();
     await DecentralizedJournal.deployed();
 
+    console.log("Deploying contracto with account:", deployer.address);
+    console.log("Account balance:", accountBalance.toString());
     console.log("Contract address:", DecentralizedJournal.address);
 }
 
